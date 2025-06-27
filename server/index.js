799f2e342a1dbde8f5914a30b3e8cc493a0f4f0c
ReitsamerSystems <office@unknown-group.com>
2025-06-27 19:06:01 +0200

    fix port for render

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // hier deine API-Routen einfügen, z.B.
 // app.post('/api/...', ...)
